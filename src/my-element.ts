7f0e112bbe767c7dca0ecbe86b3a56881764b53f
FairLadySweetie <tienhsin8841@yahoo.com.tw>
2019-03-14 02:56:52 +0800

    color

--- src/my-element.ts
+++ src/my-element.ts
@@ -3,10 +3,44 @@ import { LitElement, html, customElement, property } from 'lit-element';
 @customElement('my-element')
 export class MyElement extends LitElement {
     @property()
-    foo = 'foo';
+    foo = {"firstName":"Jayce",
+            "lastName":"Hauck",
+            "username":"Domenic Kuhn81",
+            "email":"Gladyce.Weber@yahoo.com"};
 
     render() {
-        return html`<p>${this.foo}</p>`;
+        return html`
+        <style> 
+        .s1{
+            color:Indigo  ;
+            font-size: 25px;
+            background-color: LightCoral  ;
+            height: 30px;
+            width: 200px;
+            border-radius: 10px;
+        }
+        .s2{
+            color:Indigo   ;
+            font-size: 25px;
+            background-color: LightCoral ;
+            height: 30px;
+            width: 200px;
+            border-radius: 10px;
+        }
+        .s3{
+            color:RebeccaPurple ;
+            font-size: 20px;
+            background-color: lightpink;
+            height: 30px;
+            width: 300px;
+            border-radius: 10px;
+        }
+        </style>
+        <h1 class="s1" style="font-family:sans-serif;">   ${this.foo.firstName}</h1>
+        <h2 class="s2" style="font-family:sans-serif;">   ${this.foo.lastName}</h2>
+        <p class="s3" style="font-family:sans-serif;">  ${this.foo.email}</p>
+        `
+        ;
     }
 
     firstUpdated(changedProperties:any) {
@@ -15,7 +49,7 @@ export class MyElement extends LitElement {
         });
         fetch('api/user')
             .then((response) => response.json())
-            .then((bodyRes) => console.log(bodyRes));
+            .then((bodyRes) => this.foo=bodyRes);
 
     }
 }
